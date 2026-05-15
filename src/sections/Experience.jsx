d97376a581f53bc505ad6b435b85b03d0185f988
Prabhat Kumar <prabhatpk122@gmail.com>
2026-05-16 01:23:59 +0530

    Experience section commit

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -22,11 +22,11 @@ const experiences = [
     },
 ];
 
-function ExperinceItem({exp, idx, start, end, scrollYProgress, layout}) {
-    const scale = useTransform(scrollYProgress, [start, end], [0, 1]);
-    const opacity = useTransform(scrollYProgress, [start, end], [0, 1]);
-    const y = useTransform(scrollYProgress, [start, end], [idx%2 === 0 ? 30 : -30, 0]);
-    const x = useTransform(scrollYProgress, [start, end], [-24, 0]);
+    function ExperinceItem({exp, idx, start, end, scrollYProgress, layout}) {
+    const scale = useTransform(scrollYProgress, [start, end], [0, 1])
+    const opacity = useTransform(scrollYProgress, [start, end], [0, 1])
+    const y = useTransform(scrollYProgress, [start, end], [idx%2===0 ? 30 : -30, 0])
+    const x = useTransform(scrollYProgress, [start, end], [-24, 0])
 
     if(layout === "desktop") {
         return (
@@ -55,18 +55,17 @@ function ExperinceItem({exp, idx, start, end, scrollYProgress, layout}) {
                 </motion.article>
             </div>
         )
-    }
-
-    return(
+    } else if (layout === "mobile") {
+        return(
         <div className=' relative flex items-start'>
             <motion.div className='absolute -left-3.5 top-3 z-10 w-7 h-7 rounded-full bg-white shadow-[0_0_0_8px_rgba(255,255,255,0.1)]'
             style={{scale, opacity}}
             ></motion.div>
-            <motion.article className='bg-gray-900/80 backdrop-blur border border-gray-700/70 rounded-xl p-5 w-[90vw] max-w-sm ml-6 shadow-lg'
+            <motion.article className='bg-gray-900/80 backdrop-blur border border-gray-700/70 rounded-xl p-2 w-[90vw] max-w-sm ml-5 shadow-lg'
             style={{opacity, x}}
             transition={{duration: 0.4, delay: idx*0.15}}
             >
-                <h3 className='text-xl font-semibold wrap-break-word'>
+                <h3 className='text-lg font-semibold wrap-break-word'>
                     {exp.role}
                 </h3>
                 <p className='text-sm text-gray-400 mb-2 wrap-break-word'>
@@ -78,6 +77,9 @@ function ExperinceItem({exp, idx, start, end, scrollYProgress, layout}) {
             </motion.article>
         </div>
     )
+    }
+
+    
 }
 
 
@@ -115,52 +117,57 @@ const lineSize = useTransform(scrollYProgress, (v) => `${v*100}%`);
                 </h2>
                 <div className='flex flex-1 items-center justify-center px-6 pb-10'>
                     {!isMobile && (
-                        <div className=' relative w-[80%] max-w-7xl'>
-                            <div className='relative h-1.5 bg-white/15 rounded'>
+                        <div className=' relative w-full max-w-7xl'>
+                            <div className=' relative h-1.5 bg-white/15 rounded'>
                                 <motion.div className=' absolute left-0 top-0 h-1.5 bg-white rounded origin-left'
                                 style={{width: lineSize}}
                                 >
+
                                 </motion.div>
                             </div>
-                        <div className='relative flex justify-between mt-0'>
-                            {experiences.map((exp, idx) => (
-                                <ExperinceItem 
-                                key={idx}
-                                exp={exp}
-                                idx={idx}
-                                start={idx === 0 ? 0 : thresholds[idx-1]}
-                                end={thresholds[idx]}
-                                scrollYProgress={scrollYProgress}
-                                layout = "desktop"
-                                />
-                            ))}
-                        </div>
 
-                        </div>
-                    )}
-
-                    {/* {isMobile && (
-                        <div className='relative w-full max-w-md'>
-                            <div className=' absolute left-0 top-0 bottom-0 w-1.5 bg-white/15 rounded'>
-                                <motion.div className='absolute top-0 left-0 w-1.5 bg-white rounded origin-top'
-                                style={{height: lineSize}}
-                                ></motion.div>
-                            </div>
-                            <div className=' relative flex flex-col gap-10 ml-10 mt-6 pb-28'>
+                            <div className=' relative flex justify-between mt-0'>
                                 {experiences.map((exp, idx) => (
                                     <ExperinceItem 
                                     key={idx}
                                     exp={exp}
                                     idx={idx}
-                                    start={idx === 0 ? 0 : thresholds[idx-1]}
+                                    start={idx === 0 ? 0 : thresholds[idx - 1]}
                                     end={thresholds[idx]}
                                     scrollYProgress={scrollYProgress}
-                                    layout = "mobile"
-                                />
+                                    layout="desktop"
+                                    />
+                                ))}
+                            </div>
+
+                        </div>
+                    )}
+
+                    {isMobile && (
+                        <div className='relative w-full max-w-md h-screen'>
+                            <div className=' absolute left-0 top-0 bottom-0 w-1.5 bg-white/15 rounded'>
+                                <motion.div className=' absolute top-0 left-0 w-1.5 bg-white origin-top rounded'
+                                style={{height: "100vh"}}
+                                >
+                                </motion.div>
+                            </div>
+
+                            <div className=' relative flex flex-col gap-10 ml-10 mt-6 pb-28'>
+                                {experiences.map((exp, idx) => (
+                                    <ExperinceItem 
+                                        key={idx}
+                                        exp={exp}
+                                        idx={idx}
+                                        start={idx === 0 ? 0 : thresholds[idx - 1]}
+                                        end={thresholds[idx]}
+                                        scrollYProgress={scrollYProgress}
+                                        layout="mobile"
+                                    />
                                 ))}
                             </div>
                         </div>
-                    )} */}
+                    )}
+
                 </div>
             </div>
         </div>
